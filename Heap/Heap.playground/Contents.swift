060c2f858b033971fc27295c046bcb3a15b74af5
fighting300 <fighting300@163.com>
2017-10-24 21:14:00 +0800

    排序

--- Heap/Heap.playground/Contents.swift
+++ Heap/Heap.playground/Contents.swift
@@ -23,11 +23,51 @@ public struct Heap<T> {
         }
     }
     
+    public var isEmpty: Bool {
+        return elements.isEmpty
+    }
+    
+    public var count: Int {
+        return elements.count
+    }
+    
+    @inline(__always) func parentIndex(ofIndex i: Int) -> Int {
+        return (i - 1) / 2
+    }
+    
+    @inline(__always) func leftChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 1
+    }
+    
+    @inline(__always) func rightChildOfIndex(ofIndex i: Int) -> Int {
+        return 2*i + 2
+    }
+    
+    public func peek() -> T? {
+        return elements.first
+    }
+    
     mutating func shiftDown() {
         shiftDown(0, heapSize: elements.count)
     }
     
     mutating func shiftDown(_ index: Int, heapSize: Int) {
-        
+        var parentIndex = index
+        while true {
+            let leftChildIndex = self.leftChildIndex(ofIndex: parentIndex)
+            let rightChildIndex = leftChildIndex + 1
+            
+            var first = parentIndex
+            if leftChildIndex < heapSize && isOrderedBefore(elements[leftChildIndex], elements[first]) {
+                first = leftChildIndex
+            }
+            if rightChildIndex < heapSize && isOrderedBefore(elements[rightChildIndex], elements[first]) {
+                first = rightChildIndex
+            }
+            if first = parentIndex { return }
+            
+            elements.swapAt(parentIndex, first)
+            parentIndex = first
+        }
     }
 }
